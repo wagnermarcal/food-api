c03c93803636781d2f873c134833bb29da8269e1
Wagner Marçal <wagnermarcal.2008@gmail.com>
2022-01-12 09:24:04 -0300

    Implementing controllers - Without xmlwrapper

--- src/main/java/me/wagner/foodapi/api/controller/KitchenController.java
+++ src/main/java/me/wagner/foodapi/api/controller/KitchenController.java
@@ -1,6 +1,5 @@
 package me.wagner.foodapi.api.controller;
 
-import me.wagner.foodapi.api.model.KitchensXmlWrapper;
 import me.wagner.foodapi.domain.model.Kitchen;
 import me.wagner.foodapi.domain.repository.KitchenRepository;
 import org.springframework.http.MediaType;
@@ -22,11 +21,6 @@ public class KitchenController {
     @GetMapping
     public List<Kitchen> list() {
         return kitchenRepository.list();
-    }
-
-    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public KitchensXmlWrapper listXml() {
-        return new KitchensXmlWrapper(kitchenRepository.list());
     }
 
     @GetMapping("/{id}")
